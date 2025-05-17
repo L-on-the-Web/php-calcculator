16e5f328b0ce138c19b3dde2a78681fb254ce5b3
L Jackson <06ljackson@gmail.com>
2025-05-17 12:37:14 -0400

    Create index.php
    Basic input and submit(calculate) button.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+      <input type="number" name="num01" placeholder="Number one">
+      <select name="operator">
+        <option value="add">+</option>
+        <option value="subtract">-</option>
+        <option value="multiply">*</option>
+        <option value="divide">/</option>
+      </select>
+      <input type="number" name="num02" placeholder="Number two">
+      <button>Calculate</button>
+    </form>
+  </body>
+</html>
